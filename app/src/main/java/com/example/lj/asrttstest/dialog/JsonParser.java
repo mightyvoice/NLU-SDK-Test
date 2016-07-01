7dc7048d18977feb3fc824bc8f4bbe1ba4ffb9c8
Ji Li <rpbloom@gmail.com>
2016-07-01 16:24:12 -0700

    nothing

--- app/src/main/java/com/example/lj/asrttstest/dialog/JsonParser.java
+++ app/src/main/java/com/example/lj/asrttstest/dialog/JsonParser.java
@@ -5,22 +5,13 @@ package com.example.lj.asrttstest.dialog;
  */
 import android.util.Log;
 
-import com.example.lj.asrttstest.dialog.BaseDialogManager;
-
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.InputStream;
 
 /**
  * Created by lj on 16/5/23.
  */
 public class JsonParser extends BaseDialogManager{
-
-    private JSONObject actionObject = null;
-    public String textForTTS;
-    public String textForDisplay;
 
     private boolean reset;
 
